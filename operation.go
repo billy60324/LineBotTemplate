2853d1123a8589efba43a73729dac1f38017f0c3
BillyHuang(黃柏岳) <billy@changingtec.com>
2019-01-11 15:59:53 +0800

    adjust text

--- operation.go
+++ operation.go
@@ -31,15 +31,17 @@ func httpGet() string {
 
 func googleSearch(keyword string) string {
 	// URL https://www.google.com.tw/search?q=
-
+	var answer string
 	doc, err := goquery.NewDocument("https://www.google.com.tw/search?q=" + keyword)
 	log.Print("Google Search")
 	if err != nil {
 		log.Fatal(err)
 	}
 	doc.Find(".LC201b").Each(func(i int, s *goquery.Selection) {
+		answer = s.Text()
 		log.Print(s.Text())
 	})
 
-	return "I get U"
+	return answer
+	//return "I get U"
 }
