d8664eb4b21b7f0d0e960511d2525f215c73160c
BillyHuang(黃柏岳) <billy@changingtec.com>
2019-01-11 15:31:09 +0800

    testing

--- operation.go
+++ operation.go
@@ -5,7 +5,8 @@ import (
 	"log"
 	"net/http"
 	"strings"
-	//"github.com/PuerkitoBio/goquery"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func parseMessage(message string) []string {
@@ -30,14 +31,14 @@ func httpGet() string {
 
 func googleSearch(keyword string) string {
 	// URL https://www.google.com.tw/search?q=
-	/*
-		doc, err := goquery.NewDocument("https://www.google.com.tw/search?q=" + keyword)
-		if err != nil {
-			log.Fatal(err)
-		}
-		doc.Find(".LC201b").Each(func(i int, s *goquery.Selection) {
-			log.Print(s.Text())
-		})
-	*/
+
+	doc, err := goquery.NewDocument("https://www.google.com.tw/search?q=" + keyword)
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc.Find(".LC201b").Each(func(i int, s *goquery.Selection) {
+		log.Print(s.Text())
+	})
+
 	return "I get U"
 }
