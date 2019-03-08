40af1afb0a4eedf30d92a0280d11631c04b425ab
billy <billy@changingtec.com>
2019-03-08 16:36:48 +0800

    Add struct

---
+++ DBDataStruct.go
@@ -0,0 +1,8 @@
+package main
+
+type LearnTable struct {
+	Keyword   string `form:"id" json:"id" gorm:"id"`
+	Response  string `form:"log_name" json:"log_name" gorm:"column:log_name"`
+	Teacher   string `form:"create_time" json:"create_time" gorm:"column:create_time"`
+	Timestamp string `form:"create_date" json:"create_date" gorm:"column:create_date"`
+}
